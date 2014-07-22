df718c3a48ae1d9701c8183fee006b23167b627e
alex-cory <alexcoryregister@gmail.com>
2014-07-22 14:41:20 -0700

    fixing

---
+++ src/com/AleXander/DeleteMe.java
@@ -0,0 +1,49 @@
+package com.AleXander;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+
+public class DeleteMe {
+
+	public static void main(String[] args) {
+		// Vehicle Files
+		String[][] vehicleFiles = new String[][]{
+                 {"bus", "bus", "bus", "lrv", "lrv", "lrv", "lrv"}, // vehicle keys
+                 {"8xBayshore.csv", "47VanNess.csv", "49Mission.csv", "KIngleside.csv", "LTaraval.csv", "NJudah.csv", "TThird.csv"} // vehicle values
+         };
+
+         System.out.println(vehicleFiles[0][].length);
+
+
+		// // int vehicleTypes = 0; // index of first row.  ie: keys
+		// int vehicleValues = 1; // index of second row.  ie: values
+		// int rows = vehicleFiles[vehicleValues].length;
+		// int columns =
+  //       // for each Array in Arrays
+		// for (int i = 0; i < rows; i++) {
+
+  //           // for each File in Files   OR  Type in Types
+  //           for (int j = 0; j < columns; j++ )
+		// 	if (vehicleFiles[vehicleKeys] == "bus") {
+		//     	System.out.println("Bus: " + vehicleFiles[vehicleKeys][vehicleValues]);
+		// 	}
+		// 	if (vehicleFiles[vehicleKeys] == "lrv") {
+		//     	System.out.println("LRV: " + vehicleFiles[vehicleKeys][vehicleValues]);
+		// 	}
+		// }
+	}
+
+//    public static void main(String[] args){
+//        String[][] names = {
+//            {"Mr. ","Mrs. ","Ms. "},
+//            {"Smith","Jones"}};
+//
+//        // Mr. Smith
+//        System.out.println(names[0][0] +
+//                           names[1][0]);
+//        // Ms. Jones
+//        System.out.println(names[0][2] +
+//                           names[1][1]);
+//    }
+}
---
+++ src/com/AleXander/Main.java
@@ -0,0 +1,8 @@
+package com.AleXander;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
