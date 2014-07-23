4ec688aac0bbb265b52b45f740c303c09f175174
alex-cory <alexcoryregister@gmail.com>
2014-07-23 13:12:29 -0700

    updating classes

---
+++ src/com/AleXander/MuniObject.java
@@ -0,0 +1,23 @@
+package com.AleXander;/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author Iris
+Name: Iris Buschelman
+Class: CS111B -- Programming Fundamentals: Java
+CRN: 52499-001
+Assignment: MuniModel.java
+Date:
+ */
+public abstract class MuniObject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ src/com/AleXander/TransferStop.java
@@ -0,0 +1,7 @@
+package com.AleXander;
+
+/**
+ * Created by alexcory on 7/23/14.
+ */
+public class TransferStop {
+}
