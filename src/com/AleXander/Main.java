19c56576581fcb0c9a3b51c8e459c6dd93d328c7
alex-cory <alexcoryregister@gmail.com>
2014-07-23 13:33:58 -0700

    updated main

--- src/com/AleXander/Main.java
+++ src/com/AleXander/Main.java
@@ -1,8 +1,29 @@
 package com.AleXander;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+
+        public static void main(String[] args) throws FileNotFoundException {
+//        BufferedReader bayshoreInputStream = new BufferedReader(new FileReader("./8xBayshore.csv"));
+            Scanner bayshoreInputStream = new Scanner(new File("47VanNess.csv"));
+            // Scanner missionInputStream = new Scanner(new File("49Mission.csv"));
+            // Scanner inglesideInputStream = new Scanner(new File("KIngleside.csv"));
+            // Scanner taravalInputStream = new Scanner(new File("LTaraval.csv"));
+            // Scanner judahInputStream = new Scanner(new File("NJudah.csv"));
+            // Scanner tThirdInputStream = new Scanner(new File("TThird.csv"));
+
+            bayshoreInputStream.nextLine(); // skips 1st line (bad - data)
+            while (bayshoreInputStream.hasNext()) {
+                String data = bayshoreInputStream.next();
+                String values[] = data.split(",");
+                System.out.println(values[0]);
+            }
+
+//
+        }
+
 }
