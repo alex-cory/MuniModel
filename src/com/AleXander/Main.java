bb69ea8a89b8879953512849573502c997aa9d8b
Sushikat <ikatbuschel@comcast.net>
2014-07-23 23:53:15 -0700

    parsed stuff in main

--- src/com/AleXander/Main.java
+++ src/com/AleXander/Main.java
@@ -55,8 +55,7 @@ public class Main {
             lTaravalInputStream.readLine();     // LRV
             nJudahInputStream.readLine();        // LRV
             tThirdInputStream.readLine();       // LRV
-            //Don't read out the first line of the passengers b/c has no header
-//            passengersInputStream.readLine();   // Passengers
+            passengersInputStream.readLine();   // Passengers
             transferStopsInputStream.readLine();// Transfer stops
 
             // Creating a variable for each line in each file
@@ -70,7 +69,7 @@ public class Main {
             String passengersLine;
             String transferStopsLine;
 
-            // Create raw arrays and use below to parse into nested arrays
+            // Create raw arrayLists and end arrays to be used in parse into nested arrays
             ArrayList<String[]> rawBayshore = new ArrayList<String[]>();
             ArrayList<String[]> rawVanNess = new ArrayList<String[]>();
             ArrayList<String[]>  rawMission = new ArrayList<String[]>();
@@ -85,30 +84,77 @@ public class Main {
             while ((bayshoreLine = bayshoreInputStream.readLine()) != null) {
                 rawBayshore.add(bayshoreLine.split(","));
             }
+            String[] bayshoreStopNames = new String[rawBayshore.size()];
+            String[] bayshoreStopIDs = new String[bayshoreStopNames.length];
+            sortOutArrays(rawBayshore, bayshoreStopNames, bayshoreStopIDs);
             while ((vanNessLine = vanNessInputStream.readLine()) != null) {
                rawVanNess.add(vanNessLine.split(","));
             }
+            String[] vanNessStopNames = new String[rawVanNess.size()];
+            String[] vanNessStopIDs = new String[vanNessStopNames.length];
+            sortOutArrays(rawVanNess, vanNessStopNames, vanNessStopIDs);
             while ((missionLine = missionInputStream.readLine()) != null) {
                rawMission.add(missionLine.split(","));
             }
+            String[] missionStopNames = new String[rawMission.size()];
+            String[] missionStopIDs = new String[missionStopNames.length];
+            sortOutArrays(rawMission, missionStopNames, missionStopIDs);
             while ((kInglesideLine = kInglesideInputStream.readLine()) != null) {
                 rawKIngleside.add(kInglesideLine.split(","));
             }
+            String[] kInglesideStopNames = new String[rawKIngleside.size()];
+            String[] kInglesideStopIDs = new String[kInglesideStopNames.length];
+            sortOutArrays(rawKIngleside, kInglesideStopNames, kInglesideStopIDs);
             while ((lTaravalLine = lTaravalInputStream.readLine()) != null) {
                 rawLTaraval.add(lTaravalLine.split(","));
             }
+            String[] lTaravalStopNames = new String[rawLTaraval.size()];
+            String[] lTaravalStopIDs = new String[lTaravalStopNames.length];
+            sortOutArrays(rawLTaraval, lTaravalStopNames, lTaravalStopIDs);
             while ((nJudahLine = nJudahInputStream.readLine()) != null) {
                 rawNJudah.add(nJudahLine.split(","));
             }
+            String[] nJudahStopNames = new String[rawNJudah.size()];
+            String[] nJudahStopIDs = new String[nJudahStopNames.length];
+            sortOutArrays(rawNJudah, nJudahStopNames, nJudahStopIDs);
             while ((tThirdLine = tThirdInputStream.readLine()) != null) {
                 rawTThird.add(tThirdLine.split(","));
             }
+            String[] tThirdStopNames = new String[rawTThird.size()];
+            String[] tThirdStopIDs = new String[tThirdStopNames.length];
+            sortOutArrays(rawTThird, tThirdStopNames, tThirdStopIDs);
             while ((passengersLine = passengersInputStream.readLine()) != null) {
                 rawPassengers.add(passengersLine.split(","));
             }
+            String[] passengersNames = new String[rawPassengers.size()];
+            String[] passengersStartIDs = new String[passengersNames.length];
+            String[] passengersEndIDs = new String[passengersStartIDs.length];
+            sortOutArrays(rawPassengers, passengersNames, passengersStartIDs, passengersEndIDs);
             while ((transferStopsLine = transferStopsInputStream.readLine()) != null) {
                 rawTransferStops.add(transferStopsLine.split(","));
             }
+            String[] transferStopBusIDs = new String[rawTransferStops.size()];
+            String[] transferStopLRVIDs = new String[transferStopBusIDs.length];
+            String[] transferStopNames = new String[transferStopLRVIDs.length];
+            String[] transferStopRoutesToBeTransfered = new String[transferStopNames.length];
+            sortOutArrays(rawTransferStops, transferStopBusIDs, transferStopLRVIDs, transferStopNames, transferStopRoutesToBeTransfered);
+            for(int i = 0; i < transferStopBusIDs.length; i++){
+                System.out.println(transferStopBusIDs[i]);
+                System.out.println(transferStopLRVIDs[i]);
+                System.out.println(transferStopNames[i]);
+                System.out.println(transferStopRoutesToBeTransfered[i]);
+                System.out.println();
+            }
+            // Final recap of all Arrays in Existence right now is as follows
+            // stopNames[] for all routes
+            // stopIDs[] for all routes
+            // passengerNames[]
+            // passengerStartIDs[]
+            // passengerEndIDs[]
+            // transferStopBusIDs[] stopID of the actual bus stop
+            // transferStopLRVIDs[] stopID of the actual LRV stop
+            // transferStopNames[] i think this one is pretty self-explanitory
+            // transferStopRoutesToBeTransfered[] contains the routes that transfer at that transfer stop
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -162,24 +208,59 @@ public class Main {
 //    }
 
     }
-    public static void sortOutArrays(String[][] rawText, String[] one, String[] two, String[] three){
+    public static void sortOutArrays(ArrayList<String[]> rawText, String[] one, String[] two){
+        int j = 0;
+        int k = 0;
+        for(int q = 0; q < rawText.size(); q++ ){
+            for(int i = 0; i < rawText.get(q).length; i++){
+                if(i == 0){
+                    one[j] = rawText.get(q)[i];
+                    j++;
+                } else if(i == 1){
+                    two[k] = rawText.get(q)[i];
+                    k++;
+                }
+            }
+        }
+    }
+    public static void sortOutArrays(ArrayList<String[]> rawText, String[] one, String[] two, String[] three){
         int j = 0;
         int k = 0;
         int l = 0;
-        for(int q = 0; q < rawText.length; q++ ){
-            for(int i = 0; i < rawText[q].length; i++){
+        for(int q = 0; q < rawText.size(); q++ ){
+            for(int i = 0; i < rawText.get(q).length; i++){
                 if(i == 0){
-                    one[j] = rawText[q][i];
+                    one[j] = rawText.get(q)[i];
                     j++;
                 } else if(i == 1){
-                    two[k] = rawText[q][i];
+                    two[k] = rawText.get(q)[i];
                     k++;
+                } else if( i == 2){
+                    three[l] = rawText.get(q)[i];
+                    l++;
                 }
-                if(rawText[q].length == 3) {
-                    if (i == 3) {
-                        three[l] = rawText[q][i];
-                        l++;
-                    }
+            }
+        }
+    }
+    public static void sortOutArrays(ArrayList<String[]> rawText, String[] one, String[] two, String[] three, String[] four){
+        int j = 0;
+        int k = 0;
+        int l = 0;
+        int m = 0;
+        for(int q = 0; q < rawText.size(); q++ ){
+            for(int i = 0; i < rawText.get(q).length; i++){
+                if(i == 0){
+                    one[j] = rawText.get(q)[i];
+                    j++;
+                } else if(i == 1){
+                    two[k] = rawText.get(q)[i];
+                    k++;
+                } else if( i == 2){
+                    three[l] = rawText.get(q)[i];
+                    l++;
+                } else if(i == 3){
+                    four[m]= rawText.get(q)[i];
+                    m++;
                 }
             }
         }
